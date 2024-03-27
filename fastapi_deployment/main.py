05e089f63fa91e4cdc11613b26738b0957478de4
Lucifer <orangelgy@163.com>
2024-03-27 22:08:53 +0800

    Small changes on main.py.

--- fastapi_deployment/main.py
+++ fastapi_deployment/main.py
@@ -1,16 +1,15 @@
 import uvicorn
 
 
-def main(_app):
+def main():
     from app.app import app
     log_config = {
         "version": 1,
         "disable_existing_logger": False,
         "root": False,
     }
-    uvicorn.run(_app, host="0.0.0.0", port=6006, reload=False, log_config=log_config)
-    uvicorn.run(_app, host="0.0.0.0", port=6005, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=6006, reload=False, log_config=log_config)
 
 
 if __name__ == "__main__":
-    main(app)
+    main()
