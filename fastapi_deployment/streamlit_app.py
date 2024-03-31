436d168417ac7a49b0182565203ff50b76c100ed
Lucifer <orangelgy@163.com>
2024-03-31 16:08:55 +0800

    Add other two apis into streamlit entrance.

--- fastapi_deployment/streamlit_app.py
+++ fastapi_deployment/streamlit_app.py
@@ -4,9 +4,15 @@ import random
 import streamlit as st
 from loguru import logger
 
-from app.api import run_ner_service
+from app.api import run_ner_service, run_text_classification_service, run_seq2seq_service
 from common.schema.class_mapping import MODEL_CLASSES, TASK_TYPES, DATASET_TYPES
 from common.schema.input_output import InputSchema
+
+test_sample = {
+    "named_entity_recognition": "The study demonstrated a decreased level of glucocorticoid receptors (GR) in peripheral blood lymphocytes from hypercholesterolemic subjects, and an elevated level in patients with acute myocardial infarction.",
+    "text_classification": "You want to know what the writers of this movie consider funny? A robot child sees his robot parents killed (beheaded, as I recall), and then moves between their bodies calling their names. Yeah--what a comic moment. This is the worst movie I ever paid to see.",
+    "seq2seq": "Sang Lan is one of the best athletes in our country.",
+}
 
 
 def main():
@@ -30,13 +36,22 @@ def main():
     dataset_name = st.sidebar.selectbox('Select the dataset type', DATASET_TYPES.keys(),
                                         index=st.session_state.random_dataset_index)
     st.markdown(f'### You Would Like to do {task_name.replace("_", " ").title()} Task')
-    secret = st.text_input('Please enter the test query',
-                           "The study demonstrated a decreased level of glucocorticoid receptors (GR) in")
+    secret = st.text_input('Please enter the test query',test_sample[task_name])
 
     if st.button("Answer"):
         data = InputSchema(query=secret, model_type=model_type, task=task_name, dataset=dataset_name)
         logger.info("Input data is ready.")
-        result = asyncio.run(run_ner_service(data))
+        if task_name == "named_entity_recognition":
+            logger.info(f'{task_name.replace("_", " ").title()} task start...')
+            result = asyncio.run(run_ner_service(data))
+        elif task_name == "text_classification":
+            logger.info(f'{task_name.replace("_", " ").title()} task start...')
+            result = asyncio.run(run_text_classification_service(data))
+        elif task_name == "seq2seq":
+            logger.info(f'{task_name.replace("_", " ").title()} task start...')
+            result = asyncio.run(run_seq2seq_service(data))
+        else:
+            raise KeyError("Unknown task type, please try again.")
         st.write("Result:", result)
 
 
